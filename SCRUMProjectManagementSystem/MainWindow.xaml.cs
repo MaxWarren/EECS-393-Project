a71b1907379c61f1e23aa6a86406e94c4e5156f1
Max Warren <mlw53@case.edu>
2011-10-04 01:25:40 -0400

    Added Temporary Columns

--- SCRUMProjectManagementSystem/MainWindow.xaml.cs
+++ SCRUMProjectManagementSystem/MainWindow.xaml.cs
@@ -19,9 +19,34 @@ namespace SCRUMProjectManagementSystem
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<String> projs;
+        private List<String> tasks;
+        private List<String> sprints;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            projs = new String[] { "Proj1", "Proj2" }.ToList();
+            tasks = new String[] { "Task1", "Task2" }.ToList();
+            sprints = new String[] { "Sprint1", "Sprint2" }.ToList();
+
+            projectList.ItemsSource = projs;
+            taskList.ItemsSource = tasks;
+            sprintList.ItemsSource = sprints;
+        }
+
+        private void ProjectList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (taskCol.ActualWidth > 0)
+            {
+                taskCol.Width = new GridLength(0);
+                projCol.Width = new GridLength(projCol.ActualWidth / 2);
+                sprintCol.Width = new GridLength(projCol.ActualWidth * 1.5);
+            }
         }
     }
 }
