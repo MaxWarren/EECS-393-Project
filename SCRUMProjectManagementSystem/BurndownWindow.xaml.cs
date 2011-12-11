843192142cf51bb7947183b058d1c93328664d6f
Max Warren <mlw53@case.edu>
2011-12-11 18:35:25 -0500

    replaced burndown

--- SCRUMProjectManagementSystem/BurndownWindow.xaml.cs
+++ SCRUMProjectManagementSystem/BurndownWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Shapes;
+using System.Drawing;
 
 namespace SCRUMProjectManagementSystem
 {
@@ -30,116 +31,17 @@ namespace SCRUMProjectManagementSystem
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            double diameter = 8;
-            ColumnDefinition cdtemp = new ColumnDefinition();
-            cdtemp.Width = new GridLength(60, GridUnitType.Pixel);
-            grid1.ColumnDefinitions.Add(cdtemp);
-            for (int i = 0; i < datesGoal.Length; i++)
-            {
-                ColumnDefinition cd = new ColumnDefinition();
-                cd.Width = new GridLength(1, GridUnitType.Star);
-                grid1.ColumnDefinitions.Add(cd);
-            }
-            cdtemp = new ColumnDefinition();
-            cdtemp.Width = new GridLength(60, GridUnitType.Pixel);
-            grid2.ColumnDefinitions.Add(cdtemp);
-            for (int i = 0; i < datesActual.Length; i++)
-            {
-                ColumnDefinition cd = new ColumnDefinition();
-                cd.Width = new GridLength(1, GridUnitType.Star);
-                grid2.ColumnDefinitions.Add(cd);
-            }
-
-            RowDefinition rdtemp = new RowDefinition();
-            rdtemp.Height = new GridLength(diameter, GridUnitType.Pixel);
-            grid1.RowDefinitions.Add(rdtemp);
             for (int i = 0; i < goal.Length; i++)
             {
-                RowDefinition rd = new RowDefinition();
-
-                rd.Height = new GridLength(1, GridUnitType.Star);
-
-                grid1.RowDefinitions.Add(rd);
-            }
-            rdtemp = new RowDefinition();
-            rdtemp.Height = new GridLength(30, GridUnitType.Pixel);
-            grid1.RowDefinitions.Add(rdtemp);
-            rdtemp = new RowDefinition();
-            rdtemp.Height = new GridLength(diameter, GridUnitType.Pixel);
-            grid2.RowDefinitions.Add(rdtemp);
-            for (int i = 0; i < actual.Length; i++)
-            {
-                RowDefinition rd = new RowDefinition();
-                int height = 0;
-                if (i < actual.Length - 1)
-                {
-                    height = Math.Abs(actual[i + 1] - actual[i]);
-                }
-                else
-                {
-                    height = actual[i];
-                }
-                rd.Height = new GridLength(height, GridUnitType.Star);
-                grid2.RowDefinitions.Add(rd);
-            }
-            rdtemp = new RowDefinition();
-            rdtemp.Height = new GridLength(30, GridUnitType.Pixel);
-            grid2.RowDefinitions.Add(rdtemp);
-
-            for (int i = 0; i < goal.Length; i++)
-            {
-                Ellipse el = new Ellipse();
-                el.Height = diameter;
-                el.Width = diameter;
-                el.Fill = System.Windows.Media.Brushes.Black;
-                el.VerticalAlignment = System.Windows.VerticalAlignment.Top;
-                el.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-                Grid.SetRow(el, i + 1);
-                Grid.SetColumn(el, i + 1);
-                Grid.SetRowSpan(el, 500);
-                Grid.SetColumnSpan(el, 500);
-                el.Margin = new Thickness(-1 * diameter / 2);
-                grid1.Children.Add(el);
+                mainChart.Series[0].Points.AddXY(datesGoal[i], goal[i]);
             }
             for (int i = 0; i < actual.Length; i++)
             {
-                Ellipse el = new Ellipse();
-                el.Height = diameter;
-                el.Width = diameter;
-                el.Fill = System.Windows.Media.Brushes.Green;
+                mainChart.Series[1].Points.AddXY(datesActual[i], actual[i]);
                 if (actual[i] > goal[i])
                 {
-                    el.Fill = System.Windows.Media.Brushes.Red;
+                    mainChart.Series[1].Points[i].MarkerColor = Color.Red;
                 }
-                el.VerticalAlignment = System.Windows.VerticalAlignment.Top;
-                el.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-                Grid.SetRow(el, i + 1);
-                Grid.SetColumn(el, i + 1);
-                Grid.SetRowSpan(el, 500);
-                Grid.SetColumnSpan(el, 500);
-                el.Margin = new Thickness(-1 * diameter / 2);
-                grid2.Children.Add(el);
-            }
-            for (int i = 0; i < goal.Length; i+=goal.Length / 5)
-            {
-                Label label = new Label();
-                label.Content = datesGoal.ElementAt(i).Date.ToString("MM/dd");
-                label.VerticalAlignment = System.Windows.VerticalAlignment.Center;
-                label.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-                Grid.SetRow(label, goal.Length + 1);
-                Grid.SetColumn(label, i + 1);
-                Grid.SetColumnSpan(label, 5);
-                label.Margin = new Thickness(-1 * diameter, 0, 0, 0);
-                grid1.Children.Add(label);
-
-                label = new Label();
-                label.Content = Math.Round(goal.ElementAt(i), 1);
-                label.VerticalAlignment = System.Windows.VerticalAlignment.Top;
-                label.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
-                Grid.SetRow(label, i + 1);
-                Grid.SetColumn(label, 0);
-                Grid.SetRowSpan(label, 5);
-                grid1.Children.Add(label);
             }
         }
     }
